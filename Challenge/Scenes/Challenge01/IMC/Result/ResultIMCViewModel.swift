c1dcec3c1112267158c8e6c74c34fb1adfec02a0
Diogo Gaspar <diogo.gaspar@me.com>
2022-06-24 20:47:02 -0300

    Refac: setupData from IFs to switch

--- Challenge/Scenes/Challenge01/IMC/Result/ResultIMCViewModel.swift
+++ Challenge/Scenes/Challenge01/IMC/Result/ResultIMCViewModel.swift
@@ -43,35 +43,39 @@ final class ResultIMCViewModel: NSObject {
 	private func setupData() {
 		result = model.weight / (model.height * model.height) * 10000
 		
-		if result <= IMC.underWeight.rawValue {
+		switch result {
+		case let result where result <= IMC.underWeight.rawValue:
 			image = UIImage(systemName: "person.crop.circle.badge.minus")
 			color = UIColor.cyan
 			classification = "Abaixo do normal"
-		}
-		else if result <= IMC.normal.rawValue {
+			
+		case let result where result <= IMC.normal.rawValue:
 			image = UIImage(systemName: "person.crop.circle.badge.checkmark")
 			color = UIColor.systemGreen
 			classification = "Normal"
-		}
-		else if result <= IMC.overWeight.rawValue {
+			
+		case let result where result <= IMC.overWeight.rawValue:
 			image = UIImage(systemName: "person.crop.circle.badge.plus")
 			color = UIColor.blue
 			classification = "Sobrepeso"
-		}
-		else if result <= IMC.obesity1.rawValue {
+			
+		case let result where result <= IMC.obesity1.rawValue:
 			image = UIImage(systemName: "person.crop.circle.badge.exclamationmark")
 			color = UIColor.systemYellow
 			classification = "Obesidade grau 1"
-		}
-		else if result <= IMC.obesity2.rawValue {
+			
+		case let result where result <= IMC.obesity2.rawValue:
 			image = UIImage(systemName: "person.crop.circle.badge.exclamationmark")
 			color = UIColor.systemOrange
 			classification = "Obesidade grau 2"
-		}
-		else if result >= IMC.obesity3.rawValue {
+			
+		case let result where result >= IMC.obesity3.rawValue:
 			image = UIImage(systemName: "person.crop.circle.badge.exclamationmark")
 			color = UIColor.systemRed
 			classification = "Obesidade grau 3"
+			
+		default:
+			print("Impossible to compute the data in setupData method")
 		}
 	}
 }

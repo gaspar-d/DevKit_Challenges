8d1eee33f111dd0f167e0c4000fe585b05e6363a
Diogo Gaspar <diogo.gaspar@me.com>
2022-07-25 21:02:42 -0300

    Refac: moving logic from didTapPlateButton VC to VM

--- Challenge/Scenes/Challenge02/CarScenes/CarRotationResult/CarRotationViewModel.swift
+++ Challenge/Scenes/Challenge02/CarScenes/CarRotationResult/CarRotationViewModel.swift
@@ -39,85 +39,78 @@ extension CarRotationViewModel: CarRotationViewModelProtocol {
 		
 		switch digit {
 		case 1...2:
-//			weekday = "segunda-feira"
-			weekday = "Monday"
-//			weekday = 2
+			weekday = "segunda-feira"
 		case 3...4:
 			weekday = "terça-feira"
-//			weekday = 3
 		case 5...6:
 			weekday = "quarta-feira"
-//			weekday = 4
 		case 7...8:
 			weekday = "quinta-feira"
-//			weekday = 5
 		case 9:
 			weekday = "sexta-feira"
-//			weekday = 6
 		default:
 			weekday = "sexta-feira"
-//			weekday = 6
 		}
 		
-		print(#line, "----------------->", weekday)
 		return weekday
 	}
 	
 	public func getRotationPerTime(with weekday: String) -> String {
+		var weekDayAsInt: Int
+		
+		switch weekday {
+		case "segunda-feira":
+			weekDayAsInt = 2
+		case "terça-feira":
+			weekDayAsInt = 3
+		case "quarta-feira":
+			weekDayAsInt = 4
+		case "quinta-feira":
+			weekDayAsInt = 5
+		case "sexta-feira":
+			weekDayAsInt = 6
+			
+		default:
+			fatalError("ERROR: Can't get a weekday number")
+		}
 		
 		var rotationDay: String = ""
 		
-		guard let test = Calendar.current.dateComponents([.weekday], from: .now).weekday else { return "não pegou o dia de hoje"}
-		print(#line, "----------------->", test, type(of: test))
+		guard var systemWeekday = Calendar.current.dateComponents([.weekday], from: .now).weekday else { return "não pegou o dia de hoje"}
 		
-		guard var current = Calendar.current.date(byAdding: .weekday,
-												  value: 0,
-												  to: .now)
-		else {
-			return "ERROR: cannot fetch system time"
-		}
 		
-		let dateFormatter = DateFormatter()
-		dateFormatter.dateFormat = "EEEE"
-		var systemWeekday = dateFormatter.string(from: current)
-		print(#line, "----------------->", systemWeekday)
-		
-		if weekday == systemWeekday {
+		if weekDayAsInt == systemWeekday {
 			rotationDay = "Hoje"
 		}
 		
-		if weekday != systemWeekday {
-			current = Calendar.current.date(byAdding: .weekday, value: 1, to: .now)!
-			systemWeekday = dateFormatter.string(from: current)
+		if weekDayAsInt != systemWeekday {
+			systemWeekday += 1
 			
-			if weekday == systemWeekday {
+			if weekDayAsInt == systemWeekday {
 				rotationDay = "Amanhã"
 			}
 		}
 
-		if weekday != systemWeekday {
-			current = Calendar.current.date(byAdding: .weekday, value: 2, to: .now)!
-			systemWeekday = dateFormatter.string(from: current)
+		if weekDayAsInt != systemWeekday {
+			systemWeekday += 1
 			
-			if weekday == systemWeekday {
+			if weekDayAsInt == systemWeekday {
 				rotationDay = "Depois de Amanhã"
 			}
 		}
 
-		if weekday != systemWeekday {
-			current = Calendar.current.date(byAdding: .weekday, value: 3, to: .now)!
-			systemWeekday = dateFormatter.string(from: current)
+		if weekDayAsInt != systemWeekday {
+			systemWeekday += 1
 			
-			if weekday == systemWeekday {
+			if weekDayAsInt == systemWeekday {
 				rotationDay = "Daqui a três dias"
 			}
 		}
 		
-		if weekday != systemWeekday {
-			rotationDay = "rotationDay vazio"
+		if weekDayAsInt != systemWeekday {
+			rotationDay = ""
 		}
 		
-		print(#line, "----------------->", rotationDay)
 		return rotationDay
 	}
 }

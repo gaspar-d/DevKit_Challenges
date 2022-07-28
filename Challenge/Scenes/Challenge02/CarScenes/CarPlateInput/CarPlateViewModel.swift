308a9cb601b6616b6c8f700e71739022890fc007
Diogo Gaspar <diogo.gaspar@me.com>
2022-07-28 15:43:10 -0300

    Refactoring isInputValid IFs logic to use guard instead

--- Challenge/Scenes/Challenge02/CarScenes/CarPlateInput/CarPlateViewModel.swift
+++ Challenge/Scenes/Challenge02/CarScenes/CarPlateInput/CarPlateViewModel.swift
@@ -32,20 +32,19 @@ extension CarPlateViewModel: CarPlateViewModelProtocol {
 	
 	public func isInputValid(with plate: String, controller: UIViewController) {
 		
-		if validator.isPlateFieldEmpty(with: plate) {
+		guard validator.isPlateFieldEmpty(with: plate) else { return }
 			alert.popup(title: "Campo vazio",
 						message: "Insira a numeração de sua placa para prosseguir",
 						controller: controller)
-		}
-		if validator.isPlateValidLength(with: plate) {
+		
+		guard validator.isPlateValidLength(with: plate) else { return }
 			alert.popup(title: "Placa com números faltantes",
 						message: "Ambas placas modelo Mercosul e antiga precisam de 7 dígitos",
 						controller: controller)
-		}
-		if validator.isPlateFormatCorrect(with: plate) {
+		
+		guard validator.isPlateFormatCorrect(with: plate) else { return }
 			alert.popup(title: "Placa em formato inválido",
 						message: "Placas tem formato: \n (mercosul) XXX 0X00\n (antigo) XXX 0000",
 						controller: controller)
-		}
 	}
 }
